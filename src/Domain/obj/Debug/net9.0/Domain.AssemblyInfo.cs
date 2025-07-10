e65931411a07e43f51ddfca52c33f09a3c39ca46
ilianis <ilianissp011@gmail.com>
2025-07-10 12:12:59 -0500

    se muestra los  model  en shema

--- src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb3957a7cffae29309e800456546bb2526845d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b8c0a1c2910e9191bb86a54b9143fb0ce04842")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
