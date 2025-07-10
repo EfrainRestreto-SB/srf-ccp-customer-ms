97f387217d3376ca6457b5dde7ec541a9930ae66
ilianis <ilianissp011@gmail.com>
2025-07-10 12:09:11 -0500

    no message

--- src/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ src/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62867be7a9a7c515fcc02e4281f0acb882af82bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fac4a65558aa5d21fa8adfc1b8a91eb88ad2421")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
